dcfdc863442bb9994ef636f85b7b49da572b250b
matt.r.stevenson <matt.r.stevenson@91c7b928-5bab-4dd6-8ad4-947ba1ac68b8>
2013-10-06 01:23:57 +0000

    [maven-release-plugin]  copy for tag org.rwshop.project.root-0.9.2

---
+++ tags/org.rwshop.project.root-0.9.2/maven/projects/org.rwshop.nb.animation/src/main/java/org/rwshop/nb/animation/cookies/AnimationSaveAsCookie.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2011 Hanson Robokind LLC.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.rwshop.nb.animation.cookies;
+
+import org.rwshop.swing.animation.actions.FileAction.Save;
+import org.robokind.api.animation.editor.AnimationEditor;
+import org.robokind.api.common.utils.RKSource.SourceImpl;
+import org.rwshop.nb.common.cookies.SaveAsCookie;
+
+/**
+ *
+ * @author Matthew Stevenson <www.robokind.org>
+ */
+public class AnimationSaveAsCookie extends SourceImpl<AnimationEditor> implements SaveAsCookie{
+    public AnimationSaveAsCookie(AnimationEditor controller){
+        super(controller);
+    }
+    
+    @Override
+    public void saveAs(){
+        new Save(this, true).actionPerformed(null);
+    }
+}
---
+++ tags/org.rwshop.project.root-0.9.2/maven/projects/org.rwshop.swing.messaging/src/main/java/org/rwshop/swing/messaging/Activator.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2011 Hanson Robokind LLC.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.rwshop.swing.messaging;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ *
+ * @author Matthew Stevenson <www.robokind.org>
+ */
+public class Activator implements BundleActivator {
+    private final static Logger theLogger = LoggerFactory.getLogger(Activator.class);
+    
+    @Override
+    public void start(final BundleContext context) throws Exception {
+//        theLogger.info("AQServiceSwingUI Activation Begin.");
+//        
+//        try {
+//            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+//        } catch (Exception ex) {
+//            java.util.logging.Logger.getLogger(AvroTableDemoFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+//        }
+//        
+//        java.awt.EventQueue.invokeLater(new Runnable() {
+//
+//            public void run() {
+//                AvroTableDemoFrame frame = new AvroTableDemoFrame();
+//                frame.start(context);
+//                frame.setVisible(true);
+//            }
+//        });
+//        theLogger.info("AQServiceSwingUI Activation Complete.");
+    }
+    
+    @Override
+    public void stop(BundleContext context) throws Exception {
+    }
+}
